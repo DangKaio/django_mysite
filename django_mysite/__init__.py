825d01c32bf856a52283264dde227e65d239179a
Dangkai <1370465454@qq.com>
2018-08-01 09:51:32 +0800

    Initial commit

---
+++ django_mysite/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Author: Dang Kai
+# @Date: 2018-07-31 16:30:03
+# @Last Modified time: 2018-07-31 17:38:31
+# @E-mail: 1370465454@qq.com
+# @Description: 
