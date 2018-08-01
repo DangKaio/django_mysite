2681942550a70401ef9d1143f55094424b3aa5b5
Dangkai <1370465454@qq.com>
2018-08-01 16:34:24 +0800

    改良url 视图为通用

--- Login/urls.py
+++ Login/urls.py
@@ -2,16 +2,23 @@
 # -*- coding: utf-8 -*-
 # @Author: Dang Kai
 # @Date: 2018-08-01 11:10:05
-# @Last Modified time: 2018-08-01 16:13:40
+# @Last Modified time: 2018-08-01 16:32:31
 # @E-mail: 1370465454@qq.com
 # @Description: 
 from django.urls import path
 
 from . import views
 app_name='polls'#命名空间
+# urlpatterns = [
+#     path('', views.index, name='index'),
+#     path('specifics/<int:question_id>/', views.detail, name='detail'),
+#     path('<int:question_id>/results/', views.results, name='results'),
+#     path('<int:question_id>/vote/', views.vote, name='vote'),
+# ]
+#改良urlconf
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('specifics/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- Login/views.py
+++ Login/views.py
@@ -8,51 +8,68 @@ from django.shortcuts import render,get_object_or_404
 from django.http import Http404
 from django.urls import reverse
 from .models import Choice,Question
+from django.views import generic
 
 
-def index(request):
-    # '''
-    # 默认页面http://127.0.0.1:8000/Login/
-    # 返回你是, What's up?
-    # '''
-    # 1）第一个页面
-    # # return HttpResponse("hello world ,you're at the login index")
-    # 2）第二个页面
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    # 3）第三个页面
-    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
-    # context = {
-    #     'latest_question_list': latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-    # 4）A shortcut: render() 的使用
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     # '''
+#     # 默认页面http://127.0.0.1:8000/Login/
+#     # 返回你是, What's up?
+#     # '''
+#     # 1）第一个页面
+#     # # return HttpResponse("hello world ,you're at the login index")
+#     # 2）第二个页面
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # output = ', '.join([q.question_text for q in latest_question_list])
+#     # return HttpResponse(output)
+#     # 3）第三个页面
+#     # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     # template = loader.get_template('polls/index.html')
+#     # context = {
+#     #     'latest_question_list': latest_question_list,
+#     # }
+#     # return HttpResponse(template.render(context, request))
+#     # 4）A shortcut: render() 的使用
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
-    '''404error'''
-    # return HttpResponse("You're looking at question %s." % question_id)
-    # 2)
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question': question})
-    # 3)A shortcut: get_object_or_404()使用
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-def results(request, question_id):
-    # response = "You're looking at the results of question %s."
-    # return HttpResponse(response % question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+# def detail(request, question_id):
+#     '''404error'''
+#     # return HttpResponse("You're looking at question %s." % question_id)
+#     # 2)
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+#     # 3)A shortcut: get_object_or_404()使用
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
+# def results(request, question_id):
+#     # response = "You're looking at the results of question %s."
+#     # return HttpResponse(response % question_id)
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+#改良之后
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 def vote(request, question_id):
     # return HttpResponse("You're voting on question %s." % question_id)
     question = get_object_or_404(Question, pk=question_id)
