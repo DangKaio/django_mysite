4f10a44b052e33df2911229eacea59b77a63abe0
Dangkai <1370465454@qq.com>
2018-08-02 09:47:27 +0800

    补充测试
    增加全面测试，并附带结果

--- Login/tests.py
+++ Login/tests.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Author: Dang Kai
 # @Date: 2018-08-01 11:10:05
-# @Last Modified time: 2018-08-02 09:41:23
+# @Last Modified time: 2018-08-02 09:44:42
 # @E-mail: 1370465454@qq.com
 # @Description:
 
@@ -11,7 +11,7 @@ from django.test import TestCase
 from django.utils import timezone
 from .models import Choice,Question
 
-
+#三个测试来确保 Question.was_published_recently() 方法对于过去，最近，和将来的三种情况都返回正确的值。
 class QuestionModelTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
@@ -22,3 +22,19 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+    def test_was_published_recently_with_recent_question(self):
+        """
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
--- ReadMe.md
+++ ReadMe.md
@@ -2,3 +2,24 @@
 ---
 参考资料 官网文档https://docs.djangoproject.com/en/2.0/intro/tutorial03/
 
+
+#自动化测试运行结果:
+---
+Creating test database for alias 'default'...
+System check identified no issues (0 silenced).
+.
+----------------------------------------------------------------------
+Ran 1 test in 0.000s
+
+OK
+Destroying test database for alias 'default'...
+
+D:\Software\Project_Python\django_mysite>python manage.py test Login
+Creating test database for alias 'default'...
+System check identified no issues (0 silenced).
+...
+----------------------------------------------------------------------
+Ran 3 tests in 0.001s
+
+OK
+Destroying test database for alias 'default'...
